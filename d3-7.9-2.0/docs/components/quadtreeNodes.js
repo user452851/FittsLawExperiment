53b3cb59e06a99fa1c99c9cdebb0f80961c064a8
user452851 <122833244+user452851@users.noreply.github.com>
2024-11-04 02:26:24 -0800

    dd

--- d3-7.9-2.0/docs/components/quadtreeNodes.js
+++
@@ -1,5 +0,0 @@
-export default function quadtree_nodes() {
-  const nodes = [];
-  this.visit((node, x1, y1, x2, y2) => void nodes.push({x1, y1, x2, y2}));
-  return nodes;
-}
--- d3-7.9-2.0/docs/data/volcano.data.js
+++
@@ -1,8 +0,0 @@
-import fs from "node:fs";
-
-export default {
-  watch: ["../public/data/volcano.json"],
-  load([file]) {
-    return JSON.parse(fs.readFileSync(file, "utf-8"));
-  }
-};
